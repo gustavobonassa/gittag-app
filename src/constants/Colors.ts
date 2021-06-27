ca83338c49c60a97eb676b3678079b22624c518d
Gustavo Bonassa <gustavo.bonassa1@gmail.com>
2021-06-27 20:30:03 -0300

    feat: app constants

---
+++ src/constants/Colors.ts
@@ -0,0 +1,29 @@
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
+
+export default {
+  light: {
+    text: "#000",
+    background: "#fff",
+    tint: tintColorLight,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorLight,
+    repositoriesList: "#fff",
+    searchBar: "#fff",
+    searchBarInput: "rgba(0, 0, 0, 0.07)",
+    placeholder: "rgba(0, 0, 0, .3)",
+    repositoriesItem: "#eee",
+  },
+  dark: {
+    text: "#fff",
+    background: "#000",
+    tint: tintColorDark,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorDark,
+    repositoriesList: "#333",
+    searchBar: "#222",
+    searchBarInput: "#666",
+    placeholder: "rgba(255, 255, 255, .7)",
+    repositoriesItem: "#555",
+  },
+};
