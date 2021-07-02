9a8496f69f1e0985861d0500b0b63e663d9f17f7
Gustavo Bonassa <gustavo.bonassa1@gmail.com>
2021-07-01 21:10:15 -0300

    test: new tests to Input component

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,20 +1,111 @@
-import renderer from 'react-test-renderer';
-import ReactTestUtils from 'react-dom/test-utils';
-
+import { mount, shallow } from "enzyme";
 import * as React from "react";
+import { View } from "react-native";
 
 import Input from "./Input";
 
 describe("Input success tests", () => {
   it("renders without crashing", () => {
-    const component = renderer.create(
+    const component = mount(<Input />);
+
+    expect(component).toHaveLength(1);
+  });
+
+  it("respects style prop", () => {
+    const component = mount(
+      <Input
+        style={{
+          backgroundColor: "purple",
+        }}
+      />,
+    );
+    const input = component.find("TextInput");
+    const style = input.prop("style") as any;
+
+    expect(style[2].backgroundColor).toBe("purple");
+  });
+
+  it("should allow changes to its value", () => {
+    const component = shallow(
       <Input
         value=""
-        onChange={() => ({})}
-        autoFocus
+        onChangeText={(e) => {
+          component.setProps({
+            children: <Input value={e} testID="input-children" />,
+          });
+        }}
       />
     );
+    const input = component.findWhere(
+      (node) => node.prop("testID") === "input-test"
+    );
+    input.simulate("changeText", "Hello World");
 
-    expect(component?.root?.children?.length).toBe(1);
+    const inputChildren = component.findWhere(
+      (node) => node.prop("testID") === "input-children"
+    );
+    expect(inputChildren.props().value).toEqual("Hello World");
+  });
+
+  it("respects onChangeText prop", () => {
+    const func = jest.fn();
+    const component = shallow(<Input value="" onChangeText={func} />);
+    const input = component.findWhere(
+      (node) => node.prop("testID") === "input-test"
+    );
+    input.simulate("changeText");
+    expect(func).toBeCalled();
+  });
+
+  it("respects placeholder prop", () => {
+    const component = mount(<Input placeholder="Hello World" />);
+    const findInput = component.find("TextInput");
+    expect(findInput.prop("placeholder")).toBe("Hello World");
+  });
+
+  it("respects autoFocus prop", () => {
+    const component = mount(<Input autoFocus />);
+    const findInput = component.find("TextInput");
+    expect(findInput.prop("autoFocus")).toBe(true);
+  });
+
+  it("respects disabled prop", () => {
+    const component = mount(<Input disabled />);
+    const findInput = component.find("TextInput");
+    const style = findInput.prop("style") as any;
+
+    expect(findInput.prop("editable")).toBe(false);
+
+    expect(style[1].backgroundColor).toBe("rgba(0, 0, 0, .05)");
+    expect(style[1].color).toBe("rgba(0, 0, 0, .3)");
+  });
+
+  it("should render an icon", () => {
+    const component = mount(
+      <Input
+        Icon={() => (
+          <View testID="my-icon" />
+        )}
+      />
+    );
+    const findIcon = component.findWhere(
+      (node) => node.prop("testID") === "my-icon"
+    );
+    expect(findIcon.exists()).toBe(true);
+  });
+
+  it("respects the style that Input sent to the icon", () => {
+    const component = mount(
+      <Input
+        Icon={(props: any) => (
+          <View testID="my-icon" {...props} />
+        )}
+      />
+    );
+    const findIcon = component.findWhere(
+      (node) => node.prop("testID") === "my-icon"
+    );
+    const styleProp = findIcon.first().prop("style");
+    expect(styleProp.position).toBe("absolute");
   });
 });
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,30 +2,22 @@ import React from "react";
 import {
   TextInput,
   View,
-  Text,
-  Platform,
   StyleProp,
   TextInputProps,
   TextStyle,
 } from "react-native";
 import styles from "./Input.style";
-import { Ionicons } from "@expo/vector-icons";
 
 interface IInput extends TextInputProps {
-  dark?: boolean;
   style?: StyleProp<TextStyle>;
   inputRef?: any;
   disabled?: boolean;
-  error?: string;
-  mask?: string;
   Icon?: any;
 }
 
 function Input(props: IInput) {
-  const { inputRef, disabled, error, style, dark, Icon } = props;
+  const { inputRef, disabled, style, Icon } = props;
 
-  const color = dark ? "hsla(0, 0%, 0%, .07)" : "white";
-  const ios = Platform.OS === "ios";
   const hasIcon = !!Icon;
 
   return (
@@ -36,19 +28,16 @@ function Input(props: IInput) {
         placeholderTextColor="rgba(0, 0, 0, .3)"
         placeholder=""
         {...props}
+        testID="input-test"
         editable={!disabled}
         ref={inputRef}
         style={[
           styles.input,
-          ios ? { backgroundColor: color } : null,
           disabled ? styles.disabled : null,
-          error ? styles.error : null,
           style,
           hasIcon ? { paddingLeft: 40 } : null,
         ]}
       />
-
-      {error ? <Text style={styles.errorMessage}>{error}</Text> : null}
     </View>
   );
 }
